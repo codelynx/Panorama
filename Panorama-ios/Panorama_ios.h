817dd1812feb57aabc7545ccbaef514a0fd83f10
Kaz Yoshikawa <kaz.yoshikawa@gmail.com>
2021-02-27 00:42:03 -0500

    Added Framework targets

---
+++ Panorama-ios/Panorama_ios.h
@@ -0,0 +1,19 @@
+//
+//  Panorama_ios.h
+//  Panorama-ios
+//
+//  Created by Kaz Yoshikawa on 2/27/21.
+//  Copyright © 2021 Electricwoods LLC., Kaz Yoshikawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Panorama_ios.
+FOUNDATION_EXPORT double Panorama_iosVersionNumber;
+
+//! Project version string for Panorama_ios.
+FOUNDATION_EXPORT const unsigned char Panorama_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Panorama_ios/PublicHeader.h>
+
+
